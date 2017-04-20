c7d4427c5e986ba4f54d14be66c6df1e18bbf933
ngxson <thichthat@gmail.com>
2017-04-21 00:21:12 +0700

    teacher check up

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,7 +1,16 @@
 #include "mainwindow.h"
 #include "playwindow.h"
 #include "ui_mainwindow.h"
+#include <QGraphicsPixmapItem>
+#include <QGraphicsScene>
+#include <QPixmap>
+#include <QTimer>
 #include <QDebug>
+
+#define INTRO_WIDTH 451
+
+QGraphicsPixmapItem *intro_bg[2];
+QGraphicsPixmapItem *notes[2];
 
 MainWindow::MainWindow(QWidget *parent) :
     QMainWindow(parent),
@@ -9,8 +18,38 @@ MainWindow::MainWindow(QWidget *parent) :
 {
     ui->setupUi(this);
     this->statusBar()->setSizeGripEnabled(false);
-    ui->btnPlay->setStyleSheet("border-image:url(:/img/images/ingame_bg.png);");
-    //ui->graphicsView->setScene();
+    //ui->btnPlay->setStyleSheet("border-image:url(:/img/images/ingame_bg.png);");
+
+    //Load background
+    intro_bg[0] = new QGraphicsPixmapItem();
+    intro_bg[0]->setPixmap(QPixmap(":/img/images/intro_bg.png"));
+    intro_bg[0]->setPos(-INTRO_WIDTH, 0);
+    intro_bg[1] = new QGraphicsPixmapItem();
+    intro_bg[1]->setPixmap(QPixmap(":/img/images/intro_bg.png"));
+    intro_bg[1]->setPos(0, 0);
+    notes[0] = new QGraphicsPixmapItem();
+    notes[0]->setPixmap(QPixmap(":/img/images/intro_notes.png"));
+    notes[0]->setPos(-INTRO_WIDTH, 0);
+    notes[1] = new QGraphicsPixmapItem();
+    notes[1]->setPixmap(QPixmap(":/img/images/intro_notes.png"));
+    notes[1]->setPos(0, 0);
+    QGraphicsPixmapItem *logo = new QGraphicsPixmapItem();
+    logo->setPixmap(QPixmap(":/img/images/intro_logo.png"));
+    logo->setPos(130, 0);
+
+    QGraphicsScene * scene = new QGraphicsScene();
+    scene->addItem(intro_bg[0]);
+    scene->addItem(intro_bg[1]);
+    scene->addItem(notes[0]);
+    scene->addItem(notes[1]);
+    scene->addItem(logo);
+    ui->graphicsView->setHorizontalScrollBarPolicy(Qt::ScrollBarAlwaysOff);
+    ui->graphicsView->setVerticalScrollBarPolicy(Qt::ScrollBarAlwaysOff);
+    ui->graphicsView->setScene(scene);
+
+    QTimer * timer = new QTimer();
+    connect(timer,SIGNAL(timeout()),this,SLOT(render_main()));
+    timer->start(15);
 }
 
 MainWindow::~MainWindow()
@@ -26,4 +65,21 @@ void MainWindow::on_btnPlay_clicked()
     this->hide();
     playwindow *p = new playwindow();
     p->view.show();
+}
+
+void MainWindow::render_main() {
+    //Render background
+    for (int x=0 ; x < 2 ; x++) {
+        if (intro_bg[x]->x() > INTRO_WIDTH) {
+            intro_bg[x]->setPos(-INTRO_WIDTH+1,0);
+        } else {
+            intro_bg[x]->setPos(intro_bg[x]->x()+1,0);
+        }
+
+        if (notes[x]->x() > INTRO_WIDTH) {
+            notes[x]->setPos(-INTRO_WIDTH+2,0);
+        } else {
+            notes[x]->setPos(notes[x]->x()+2,0);
+        }
+    }
 }
--- mainwindow.h
+++ mainwindow.h
@@ -18,6 +18,7 @@ public:
 
 private slots:
     void on_btnPlay_clicked();
+    void render_main();
 
 private:
     Ui::MainWindow *ui;
